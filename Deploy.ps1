412f1d47e74f40a390112655961240eb80d9de82
Tom Cumbow <tcumbow@icloud.com>
2021-01-02 21:42:10 -0500

    Update Deploy.ps1

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,2 +1,2 @@
-robocopy .\AutoLoot "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\AutoLoot" /mir
-robocopy .\AutoLoot R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\AutoLoot /mir
+robocopy .\AutoLoot S:\AutoLoot /mir
+robocopy .\AutoLoot J:\AutoLoot /mir
